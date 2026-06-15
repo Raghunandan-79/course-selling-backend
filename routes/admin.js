3d04d12e656a81c0f279b00be8c591f41273053a
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-06-16 01:01:48 +0530

    Added auth endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,30 +1,116 @@
+require("dotenv").config()
 const { Router } = require("express")
+const { z } = require("zod")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const JWT_ADMIN_SECRET = process.env.JWT_ADMIN_SECRET
 const adminRouter = Router()
 const { AdminModel } = require("../db")
 
-adminRouter.post("/signup", (req, res) => {
+// adminRouter.post("/signup", async (req, res) => {
+//     const requiredBody = z.object({
+//         email: z.string().min(3).max(100).email(),
+//         password: z.string().min(3).max(30).regex(
+//             /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+//             {
+//                 message:
+//                     "Password must contain 1 uppercase, 1 lowercase, 1 number and 1 special character"
+//             }
+//         ),
+//         firstName: z.string().min(3).max(30),
+//         lastName: z.string().min(3).max(30)
+//     }).strict()
 
-    res.json({
-        message: "Admin signup endpoint"
+//     const parsedDataWithSuccess = requiredBody.safeParse(req.body)
+
+//     if (!parsedDataWithSuccess.success) {
+//         return res.json({
+//             message: "Incorrect form",
+//             error: parsedDataWithSuccess.error
+//         })
+//     }
+
+//     const { email, password, firstName, lastName } = req.body
+
+//     let errorThrown = false
+
+//     try {
+//         const hashedPassword = await bcrypt.hash(password, 10)
+
+//         await AdminModel.create({
+//             email: email,
+//             password: hashedPassword,
+//             firstName: firstName,
+//             lastName: lastName
+//         })
+//     } catch (e) {
+//         res.status(400).json({
+//             message: "User with email already exits"
+//         })
+
+//         errorThrown = true
+//     }
+
+//     if (!errorThrown) {
+//         res.json({
+//             message: "Admin signup endpoint"
+//         })
+//     }
+// })
+
+adminRouter.post("/signin", async (req, res) => {
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        password: z.string().min(3).max(30)
+    }).strict()
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body)
+
+    if (!parsedDataWithSuccess.success) {
+        return res.json({
+            message: "Incorrect form",
+            error: parsedDataWithSuccess.error
+        })
+    }
+
+    const { email, password } = req.body
+
+    const user = await AdminModel.findOne({
+        email: email
     })
+
+    if (!user) {
+        return res.status(400).json({
+            message: "User doesnt exists"
+        })
+    }
+
+    const passwordMatched = await bcrypt.compare(password, user.password)
+
+    if (passwordMatched) {
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, JWT_ADMIN_SECRET)
+
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 })
 
-adminRouter.post("/signin", (req, res) => {
-    
-    res.json({
-        message: "Admin signin endpoint"
-    })
-})
+adminRouter.post("/", (req, res) => {
 
-adminRouter.post("/course", (req, res) => {
-    
     res.json({
         message: "Add course endpoint"
     })
 })
 
-adminRouter.put("/course", (req, res) => {
-    
+adminRouter.put("/", (req, res) => {
+
     res.json({
         message: "update course endpoint"
     })
--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,105 @@
+require("dotenv").config()
 const { Router } = require("express")
-
+const { z } = require("zod")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const { UserModel } = require("../db")
+const JWT_USER_SECRET = process.env.JWT_USER_SECRET
 const userRouter = Router()
 
-userRouter.post("/signup", (req, res) => {
+userRouter.post("/signup", async (req, res) => {
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        password: z.string().min(3).max(30).regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+            {
+                message:
+                    "Password must contain 1 uppercase, 1 lowercase, 1 number and 1 special character"
+            }
+        ),
+        firstName: z.string().min(3).max(30),
+        lastName: z.string().min(1).max(30)
+    }).strict()
 
-    res.json({
-        message: "signup endpoint"
-    })
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body)
+
+    if (!parsedDataWithSuccess.success) {
+        return res.json({
+            message: "Incorrect form",
+            error: parsedDataWithSuccess.error
+        })
+    }
+
+    const { email, password, firstName, lastName } = req.body
+
+    let errorThrown = false
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        await UserModel.create({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+    } catch (e) {
+        res.status(400).json({
+            message: "User with email already exits"
+        })
+
+        errorThrown = true
+    }
+
+    if (!errorThrown) {
+        res.json({
+            message: "Signup successfull"
+        })
+    }
 })
 
-userRouter.post("/signin", (req, res) => {
+userRouter.post("/signin", async (req, res) => {
+    const requiredBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        password: z.string().min(3).max(30)
+    }).strict()
 
-    res.json({
-        message: "signin endpoint"
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body)
+
+    if (!parsedDataWithSuccess.success) {
+        return res.json({
+            message: "Incorrect form",
+            error: parsedDataWithSuccess.error
+        })
+    }
+
+    const { email, password } = req.body
+
+    const user = await UserModel.findOne({
+        email: email
     })
+
+    if (!user) {
+        return res.status(400).json({
+            message: "User doesnt exists"
+        })
+    }
+
+    const passwordMatched = await bcrypt.compare(password, user.password)
+
+    if (passwordMatched) {
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, JWT_USER_SECRET)
+
+        res.json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 })
 
 userRouter.get("/purchases", (req, res) => {
